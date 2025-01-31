9d2e4b07571fc05ab53820229470052f54769c16
Keith Lewis <kclewis0614@gmail.com>
2025-01-31 15:45:10 -0600

    add base dir

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: ',/',
   plugins: [vue()],
   server: {
+    port: 8000, // 🔧 Change this to your desired port number
+    host: true, // 🌐 Ensures the server is accessible from the network
+    strictPort: true, // 🚫 Prevents the port from changing if it’s in use
+  },
+  preview: {
     port: 8000, // 🔧 Change this to your desired port number
     host: true, // 🌐 Ensures the server is accessible from the network
     strictPort: true, // 🚫 Prevents the port from changing if it’s in use
